05f507bda5c02ea9480623d3226b6cdb9daf260a
Michael Robinson <kb1dds@gmail.com>
2026-04-08 19:27:56 -0400

    First draft. Maybe works!

---
+++ qpsk_detect.py
@@ -0,0 +1,104 @@
+#!/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.fft import fft,ifft
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser(
+    prog = 'qpsk_detect.py',
+    description = 'Try to detect a QPSK signal in an SDRIQ file produced by SDRAngel')
+
+parser.add_argument('filename')
+parser.add_argument('--window_size',
+                    type=int,
+                    help='FFT window size',
+                    default=1024)
+parser.add_argument('--offset',
+                    type=int,
+                    help='Number of windows to skip before processing',
+                    default=0)
+parser.add_argument('--nomarkers',
+                    help='Disable markers on the display',
+                    action = 'store_true',
+                    default=False)
+parser.add_argument('--center',
+                    help='Desired center frequency in Hz',
+                    type=int,
+                    default=2216.5e6)
+parser.add_argument('--fftsamples',
+                    help='Number of samples in Doppler FFT',
+                    type=int,
+                    default=1024)
+
+args = parser.parse_args()
+
+window_size = args.window_size
+fftsamples = args.fftsamples
+offset = args.offset
+nomarkers = args.nomarkers
+tx_cf = args.center
+
+symbol_rates = [72e3, 2e6, 4e6, 6e6] # Possible symbol rates
+
+with open(args.filename,'rb') as fp:
+    sample_rate = np.fromfile(fp, dtype='uint32', count=1, sep='')[0]
+    center_freq = np.fromfile(fp, dtype='uint64', count=1, sep='')[0]
+    timestamp = np.fromfile(fp, dtype='uint64', count=1, sep='')[0]
+    sample_size = np.fromfile(fp, dtype='uint32', count=1, sep='')[0]
+    crc = np.fromfile(fp, dtype='uint32', count=1, sep='',offset=4)[0]
+
+    # Assess number of samples
+    data_start = fp.tell()
+    fp.seek(0,2)
+    if sample_size == 16:
+        sample_bytes = 4
+        sample_dtype = 'int16'
+        num_samples = (fp.tell()-data_start)/4
+    elif sample_size == 24: # NB apparently actually "24" means 32 bits
+        sample_bytes = 8
+        sample_dtype = 'int32'
+        num_samples = (fp.tell()-data_start)/8
+        
+    # Pull data from the file
+    fp.seek(data_start+window_size*offset*sample_bytes,0)
+    data = np.fromfile(fp,
+                       dtype=sample_dtype,
+                       sep='',
+                       count=2*window_size)
+
+    # Unpack the data into the proper complex type
+    data = np.reshape(data, (window_size,2), order='C')
+    data = data[:,0] + 1j*data[:,1]
+    data.squeeze()
+
+    # Baseband the data
+    data = data*np.exp(-1j*2*np.pi*tx_cf/sample_rate*np.arange(window_size))
+
+    # Squash the phase
+    data_sq = data**4
+
+    # FFT
+    data_smoothed = 20*np.log10(np.abs(fft(data_sq,axis=0,n=fftsamples)))
+    freq_axis=np.linspace(0,sample_rate,fftsamples)
+
+    # Peak detect
+    peak_freq = freq_axis[np.argmax(data_smoothed.squeeze())]
+
+    # Estimate Doppler from possible symbol rates
+    doppler = min([abs(peak_freq-sr) for sr in symbol_rates])
+
+    # Display
+    plt.plot(freq_axis,np.real(data_smoothed.squeeze()),'b')
+    if not nomarkers:
+        for sr in symbol_rates:
+            plt.plot([sr,sr],plt.ylim(),'r')
+        plt.plot([peak_freq,peak_freq],plt.ylim(),'c')
+    plt.plot(freq_axis,np.real(data_smoothed.squeeze()),'b')
+    plt.xlabel('Doppler frequency (Hz)')
+    plt.ylabel('Relative signal level (dB)')
+    plt.title('Doppler estimate : {} Hz'.format(doppler))
+    plt.show()
+    
+    
